7996e865d52120780d504644402918139839eb8f
João Cristóvão <jmacristovao@gmail.com>
2014-03-07 10:15:22 +0000

    Updated with mailing list suggestions

--- src/Data/Tree/Util.hs
+++ src/Data/Tree/Util.hs
@@ -3,16 +3,20 @@ module Data.Tree.Util
   (
     lookupTree
   , lookupTreeInForest
-  , find
-  , findSubTreeInForest
+  , lookupTreeBy
+  , lookupTreeInForestBy
   , filter
+  , filterPruneTree
   , filterSub
   , size
+  , maxDepth
+  , prune
   , mirror
   ) where
 
 import Prelude hiding (filter)
 import Data.Tree
+import Data.Maybe
 import qualified Data.List as L
 import qualified Data.Foldable as F
 import Data.Monoid
@@ -34,16 +38,16 @@ lookupTreeInForest v (t:ts) = case lookupTree v t of
                              Just t' -> Just t'
                              Nothing -> lookupTreeInForest v ts
 
-find :: (a -> Bool) -> Tree a -> Maybe (Tree a)
-find f t
+lookupTreeBy :: (a -> Bool) -> Tree a -> Maybe (Tree a)
+lookupTreeBy f t
   | f (rootLabel t) = Just t
-  | otherwise       = findSubTreeInForest f $ subForest t
+  | otherwise       = lookupTreeInForestBy f $ subForest t
 
-findSubTreeInForest :: (a -> Bool) -> [Tree a] -> Maybe (Tree a)
-findSubTreeInForest _ [] = Nothing
-findSubTreeInForest f (t:ts) = case find f t of
+lookupTreeInForestBy :: (a -> Bool) -> [Tree a] -> Maybe (Tree a)
+lookupTreeInForestBy _ [] = Nothing
+lookupTreeInForestBy f (t:ts) = case lookupTreeBy f t of
                              Just t' -> Just t'
-                             Nothing -> findSubTreeInForest f ts
+                             Nothing -> lookupTreeInForestBy f ts
 
 dropSubTree :: (a -> Bool) -> Tree a -> Tree a
 dropSubTree _ (Node x [])  = Node x []
@@ -53,6 +57,12 @@ dropSubTree _ (Node x [])  = Node x []
 size :: Tree a -> Int
 size = getSum . F.foldMap (const $ Sum 1)
 
+-- https://stackoverflow.com/questions/21205213/haskell-tail-recursion-version-of-depth-of-binary-tree
+maxDepth :: Tree a -> Int
+maxDepth (Node _ []) = 1
+maxDepth (Node _ fr) = 1 + maximum (map maxDepth fr)
+
+
 foldTree :: (a -> [b] -> b) -> Tree a -> b
 foldTree f (Node a ts) = f a (map (foldTree f) ts)
 
@@ -61,11 +71,21 @@ filter :: (a -> Bool) -> Tree a -> Tree a
 filter p = foldTree f
     where f a ts = Node a (L.filter (p . rootLabel) ts)
 
+-- | Prune every subtree whose root label does not match.
+filterPruneTree :: (a -> Bool) -> Tree a -> Maybe (Tree a)
+filterPruneTree p (Node x ns)
+   | p x = Just . Node x $ filterPruneForest p ns
+   | otherwise = Nothing
+
+filterPruneForest :: (a -> Bool) -> Forest a -> Forest a
+filterPruneForest = mapMaybe . filterPruneTree
+
 -- | remove all subtrees whose nodes do not fulfill predicate
-filterSub :: (a -> Bool) -> Tree a -> Tree a
-filterSub f t = Node
-                 (rootLabel t)
-                 (map (filterSub f) $ L.filter (treeany f) $ subForest t)
+filterSub :: (a -> Bool) -> Tree a -> Maybe (Tree a)
+filterSub f (Node x ns)
+  | f x = Just.  Node x $
+                 (mapMaybe (filterSub f) $ L.filter (treeany f) ns)
+  | otherwise = Nothing
 
 -- | is predicate true in any node of tree ?
 treeany :: (a -> Bool) -> Tree a -> Bool
@@ -73,3 +93,8 @@ treeany f t = f (rootLabel t) || any (treeany f) (subForest t)
 
 mirror :: Tree a -> Tree a
 mirror (Node a ts) = Node a . reverse $ map mirror ts
+
+-- | remove all nodes past a certain depth
+prune :: Int -> Tree a -> Tree a
+prune 0 t = Node (rootLabel t) []
+prune d t = Node (rootLabel t) (map (prune $ d-1) $ subForest t)
--- test/TreeUtilSpec.hs
+++ test/TreeUtilSpec.hs
@@ -39,16 +39,16 @@ spec = do
                                                 ]
                                        ]
 
-    it "find" $ do
-      find (==5) tree5   `shouldBe` Just (Node 5 [])
-      find (==5) tree6   `shouldBe` Nothing
-      find (==5) bigTree `shouldBe` Just (Node 5 [Node 6 []])
+    it "lookupTreeBy" $ do
+      lookupTreeBy (==5) tree5   `shouldBe` Just (Node 5 [])
+      lookupTreeBy (==5) tree6   `shouldBe` Nothing
+      lookupTreeBy (==5) bigTree `shouldBe` Just (Node 5 [Node 6 []])
 
     it "find result size is <= original tree size" . property
-      $ \(tr :: Tree Int) -> size tr >= (maybe 0 size . find (== 0) $ tr)
+      $ \(tr :: Tree Int) -> size tr >= (maybe 0 size . lookupTreeBy (== 0) $ tr)
 
     it "find result is a subset of original tree" . property
-      $ \(tr :: Tree Int) -> let f = find (== 0) tr
+      $ \(tr :: Tree Int) -> let f = lookupTreeBy (== 0) tr
                              in case f of
                                Nothing -> True
                                Just x  -> flatten x `L.isInfixOf` flatten tr
@@ -66,8 +66,8 @@ spec = do
                                Nothing -> True
                                Just x  -> flatten x `L.isInfixOf` flatten tr
 
-    it "lookupTree vs find" . property
-      $ \(tr :: Tree Int) -> lookupTree 0 tr == find (==0) tr
+    it "lookupTree vs lookupTreeBy" . property
+      $ \(tr :: Tree Int) -> lookupTree 0 tr == lookupTreeBy (==0) tr
 
     {-it "filter" $ do-}
       {-filter (== 0) bigTree `shouldBe` Node 0 []-}
@@ -81,16 +81,27 @@ spec = do
     it "filterSub" $ do
       {-filterSub (==3) bigTree-}
         {-`shouldBe` Node 0 [ Node 2 [ Node 4 [ Node 3 []]]]-}
-      filterSub (== 0) bigTree `shouldBe` Node 0 []
-      filterSub (/= 2) bigTree `shouldBe` Node 0 [Node 1 [Node 5 []]]
-      filterSub (== 1) bigTree `shouldBe` Node 0 [Node 1 []]
-      filterSub (== 9) bigTree `shouldBe` Node 0 []
-      filterSub (== 5) bigTree `shouldBe` Node 0 [Node 2 [Node 5 []]
-                                                 ,Node 1 [ Node 5 []]]
+      filterSub (== 0) bigTree `shouldBe` Just (Node 0 [])
+      filterSub (/= 2) bigTree `shouldBe` Just (Node 0 [Node 1 [Node 5 []]])
+      filterSub (== 1) bigTree `shouldBe` Nothing -- Node 0 [Node 1 []]
+      {-filterSub (== 9) bigTree `shouldBe` Node 0 []-}
+      {-filterSub (== 5) bigTree `shouldBe` Node 0 [Node 2 [Node 5 []]-}
+                                                 {-,Node 1 [ Node 5 []]]-}
 
-      filterSub (== 3) bigTree `shouldBe` Node 0 [ Node 2 [ Node 4 [ Node 3 []]]]
+      {-filterSub (== 3) bigTree `shouldBe` Node 0 [ Node 2 [ Node 4 [ Node 3 []]]]-}
 
-      filterSub (== 1) bigTree `shouldBe` Node 0 [Node 1 []]
+      {-filterSub (== 1) bigTree `shouldBe` Node 0 [Node 1 []]-}
+
+    it "filterSub vs filterPruneTree" . property
+      $ \(tr :: Tree Int) -> filterSub (==0) tr == filterPruneTree (== 0) tr
 
     it "size" . property
       $ \(tr :: Tree Int) -> size tr == (L.length . flatten $ tr)
+
+    it "maxDepth" $ do
+      maxDepth bigTree `shouldBe` 4
+      maxDepth tree5   `shouldBe` 1
+
+    it "prune" . property $
+      \(tr :: Tree Int,mx :: Positive Int) -> maxDepth (prune (getPositive mx) tr)
+                                              <= getPositive mx + 1
